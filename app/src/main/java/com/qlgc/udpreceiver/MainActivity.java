f505334f25960948b7f9f4d9047941fd473dc0f2
QLQL <qingju2004@gmail.com>
2016-11-10 21:59:23 +0000

    Extract the byte buffer from the received package and convert two bytes to one short. Playback to the receiver.

--- app/src/main/java/com/qlgc/udpreceiver/MainActivity.java
+++ app/src/main/java/com/qlgc/udpreceiver/MainActivity.java
@@ -1,6 +1,10 @@
 package com.qlgc.udpreceiver;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Handler;
+import android.renderscript.Sampler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,7 +24,13 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String IP_ADDRESS = "192.168.0.7";//"131.227.95.234";//"10.64.8.78";
     private boolean Server_aktiv = true;
+    private boolean playMode = true;
     private TextView textView;
+
+    private int sampleRate = 48000;//44100;
+    private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    AudioTrack player;
+    private int playBufSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, audioFormat);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +64,18 @@ public class MainActivity extends AppCompatActivity {
                 byte[] message = new byte[8000]; //You must set the incoming message's maximum size
 
                 try {
+                    if (playMode) {
+                        player = new AudioTrack(AudioManager.STREAM_MUSIC,
+                                sampleRate,
+                                AudioFormat.CHANNEL_OUT_MONO,
+                                AudioFormat.ENCODING_PCM_16BIT,
+                                playBufSize,
+                                AudioTrack.MODE_STREAM);
+                        player.setPlaybackRate(sampleRate);
+                        Log.w("VS", "Player initialized");
+                        player.play();
+                    }
+
                     int Frame = 0;
                     InetAddress serverAddr = InetAddress.getByName(IP_ADDRESS);
                     if (serverAddr.isReachable(1000))
@@ -78,6 +100,38 @@ public class MainActivity extends AppCompatActivity {
                             Frame++;
                             Log.d("Udp",String.valueOf(Frame)+" received "+String.valueOf(dp.getLength())+" data");
 
+                            // byte to short
+                            byte[] buffer = dp.getData();
+                            int Sample  = buffer.length/2;
+                            short LSB, MSB;
+                            short shortrecover;
+                            if (Sample>1){
+                                short[] shortBuffer = new short[Sample];
+                                short[] shortBuffer2 = new short[Sample];
+                                for (int i=0;i<Sample;i++){
+                                    LSB = (short) buffer[i*2];
+                                    MSB = (short) buffer[i*2+1];
+                                    if (LSB<0){
+                                        shortrecover = (short) (MSB*256 + LSB + 256);
+                                    } else {
+                                        shortrecover = (short) (MSB*256 + LSB);
+                                    }
+
+                                    shortBuffer[i] = shortrecover;
+
+                                    shortrecover = (short) ( (MSB<<8) | LSB&0xFF );
+                                    shortBuffer2[i] = shortrecover;
+                                }
+                                Log.w("UDP", "C:  short1: " + Arrays.toString(Arrays.copyOfRange(shortBuffer, 0, 50)));
+                                Log.w("UDP", "C:  short2: " + Arrays.toString(Arrays.copyOfRange(shortBuffer2, 0, 50)));
+                                Log.w("UDP", "C: read in: " + Arrays.toString(Arrays.copyOfRange(buffer, 0, 50)));
+                                if (playMode) {
+                                    player.write(shortBuffer, 0, shortBuffer.length);
+                                }
+                            }
+
+
+
 
                             // show the frame number and the length in the screen
                             final int LL = dp.getLength();
@@ -98,6 +152,10 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                     ds.close();
+                    if (playMode){
+                        player.release();
+                        Log.d("VS","Player released");
+                    }
                 } catch (IOException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
